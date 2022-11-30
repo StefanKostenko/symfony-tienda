045c070b363d040304510adfb825079ef085136b
StefanKostenko <kostenkostephan@gmail.com>
2022-11-30 10:31:52 +0100

    punto 2.2 fin

--- symfony-tienda/src/Controller/PageController.php
+++ symfony-tienda/src/Controller/PageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,33 +12,27 @@ class PageController extends AbstractController
 {
 
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/index.html.twig', []);
+        return $this->render('page/index.html.twig');
     }
 
     #[Route('/about', name: 'about')]
-    public function about(): Response
+    public function about(ManagerRegistry $doctrine): Response
     {
-        return $this->render('page/about.html.twig', []);
-    }
-
-    #[Route('/contact', name: 'contact')]
-    public function contact(): Response
-    {
-        return $this->render('page/contact.html.twig', []);
-    }
-
-    #[Route('/price', name: 'price')]
-    public function price(): Response
-    {
-        return $this->render('page/price.html.twig', []);
+        return $this->render('page/about.html.twig');
     }
 
     #[Route('/service', name: 'service')]
     public function service(): Response
     {
         return $this->render('page/service.html.twig', []);
+    }
+
+    #[Route('/price', name: 'price')]
+    public function price(): Response
+    {
+        return $this->render('page/price.html.twig', []);
     }
 
     #[Route('/team', name: 'team')]
@@ -49,6 +45,19 @@ class PageController extends AbstractController
     public function testimonial(): Response
     {
         return $this->render('page/testimonial.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    public function teamTemplate(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Team::class);
+        $team = $repository->findAll();
+        return $this->render('partials/_team.html.twig',compact('team'));
     }
 
 }
